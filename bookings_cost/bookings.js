988f9bcaa8e36dfea960bcc81c5dc97e1af0197b
Makhosandile <makhosandile@projectcodex.co>
2022-04-21 18:52:10 +0200

    pushing latests changes

---
+++ bookings_cost/bookings.js
@@ -0,0 +1,20 @@
+function bookingCost(booking) {
+    let discountFactor = 1;
+    let bookingCost = 45 * booking.duration;
+    if (booking.check_in_day == "Wednesday") {
+        discountFactor = 0.75;
+
+    }
+    else if (booking.check_in_day == "Friday") {
+        discountFactor = 0.85;
+    }
+    return bookingCost * discountFactor
+
+}
+
+// R45 * 3 = R135.00
+//   const booking = {
+//     check_in_day: 'Wednesday',
+//     duration: 3,
+//     cat_name: 'Snowy'
+// }
--- bookings_cost/test/bookings.tests.js
+++ bookings_cost/test/bookings.tests.js
@@ -1,8 +1,31 @@
-describe('Weekly booking' , function(){
-    it('' , function(){
-        assert.equal();
+describe('Weekly booking', function () {
+    it('should return the booking cost for Wednesday special of 25% off', function () {
+        const booking = {
+            check_in_day: 'Wednesday',
+            duration: 3,
+            cat_name: 'Snowy'
+        }
+        assert.equal(101.25, bookingCost(booking));
 
-        assert.deepEqual();
+    });
+
+    it('should return the booking cost for Friday special of 15% off', function () {
+       
+        assert.equal(114.75, bookingCost({
+            check_in_day: 'Friday',
+            duration: 3,
+            cat_name : 'Snowy'
+          }));
+
+    });
+
+    it('should return the booking cost for Monday', function () {
+        assert.equal(135, bookingCost({
+            check_in_day: 'Monday',
+            duration: 3,
+            cat_name : 'Snowy'
+          }));
+
     });
 
 });
