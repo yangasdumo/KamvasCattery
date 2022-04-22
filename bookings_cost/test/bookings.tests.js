7f119e52d2586da58c4b9df7da09f78221199b05
Yanga <yangasiqiki@gmail.com>
2022-04-22 16:07:58 +0200

    2nd commit

--- bookings_cost/test/bookings.tests.js
+++ bookings_cost/test/bookings.tests.js
@@ -1,1 +1,11 @@
-
+describe('test bookingsCost', function (){
+it('it should return R135.00 if bookingCost * discountFactor on Monday',function(){
+    assert.equal();
+});
+it('it should return 101.25 if bookingCost * discountFactor on Wednesday',function(){
+    assert.equal( );
+});
+it('it should return 114.75 if bookingCost * discountFactor on Friday',function(){
+    assert.equal( );
+});
+});
